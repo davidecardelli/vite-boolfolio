ccae10ce0ee17cf8e8d67a1abfbaf6bfce61c0da
davidecardelli <davide.cardelli.94@gmail.com>
2023-03-17 16:55:58 +0100

    creo e stampo le card ricevute tramite API

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,28 @@
 import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
 import AppFooter from './components/AppFooter.vue';
+import AppCard from './components/AppCard.vue';
+const apiBaseUrl = 'http://127.0.0.1:8000/api/'
 
 export default {
     name: 'App',
-    components: { AppHeader, AppFooter },
-    data() {
-        return {}
-    },
+    components: { AppHeader, AppFooter, AppCard },
+    data: () => ({
+        projects: []
+    }),
     props: {
     },
     computed: {
     },
     methods: {
+        fetchPosts() {
+            axios.get(apiBaseUrl + 'projects').then(res => {
+                this.projects = res.data
+            })
+        }
+    },
+    created() {
+        this.fetchPosts()
     },
     emits: [],
 }
@@ -21,8 +31,21 @@ export default {
 
 <template>
     <app-header></app-header>
-
+    <main class="my-5">
+        <div class="container">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-xl-6">
+                <app-card v-for="project in projects" :project="project"></app-card>
+            </div>
+        </div>
+    </main>
     <app-footer></app-footer>
 </template>
 
-<style></style>
+<style>
+#app {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+</style>
---
+++ src/components/AppCard.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: 'AppCard',
+    components: {},
+    data() {
+        return {}
+    },
+    props: {
+        project: Object
+    },
+    computed: {
+    },
+    methods: {
+    },
+    emits: [],
+}
+</script>
+
+<template>
+    <!-- Start AppCard Component -->
+    <div class="col mb-3">
+        <div class="card d-flex flex-column align-items-center justify-content-center h-100">
+            <a href="#">
+                <img class="p-3 card-img-top" :src="project.image" :alt="project.title">
+            </a>
+            <div class="card-body">
+                <p class="text-center text-black mt-2 fw-bold text-uppercase">{{ project.title }}</p>
+            </div>
+        </div>
+    </div>
+    <!-- End AppCard Component -->
+</template>
+
+
+
+<style lang="scss" scoped></style>
